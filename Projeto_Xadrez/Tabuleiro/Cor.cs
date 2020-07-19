865ef84fe52bcf3c814ac7f4b381b2d9cdc20038
Diego Sumiya <diego.sumiya@gmail.com>
2020-07-19 15:20:00 -0300

    Comecando a implementar o tabuleiro

---
+++ Projeto_Xadrez/Tabuleiro/Cor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto_Xadrez.Tabuleiro
+{
+    enum Cor
+    {
+        Branca,
+        Preta,
+        Amarela,
+        Azul,
+        Vermelho,
+        Verde,
+        Laranja,
+    }
+}
